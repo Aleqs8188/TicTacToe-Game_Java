02e0980f5ca627867c33b56cef71c291e400e7e5
Aleqs8188 <alexbulgak70@gmail.com>
2023-04-13 00:04:34 +0200

    Finished commit

--- src/main/java/pl/oleksii/GameBoard.java
+++ src/main/java/pl/oleksii/GameBoard.java
@@ -1,2 +1,78 @@
-package pl.oleksii;public class GameBoard {
+package pl.oleksii;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GameBoard {
+    public static ArrayList<Integer> playerPositions = new ArrayList<>();
+    public static ArrayList<Integer> cpuPositions = new ArrayList<>();
+    public static char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '}};
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] c : gameBoard) {
+            System.out.println(c);
+        }
+        System.out.println();
+    }
+
+    public static void PlacePiece(char[][] gameBoard, int pos, String user) {
+        char symbol = ' ';
+
+        if (user.equals("player")) {
+            symbol = 'X';
+            playerPositions.add(pos);
+        } else if (user.equals("cpu")) {
+            cpuPositions.add(pos);
+            symbol = 'O';
+        }
+
+        switch (pos) {
+            case 1 -> gameBoard[0][0] = symbol;
+            case 2 -> gameBoard[0][2] = symbol;
+            case 3 -> gameBoard[0][4] = symbol;
+            case 4 -> gameBoard[2][0] = symbol;
+            case 5 -> gameBoard[2][2] = symbol;
+            case 6 -> gameBoard[2][4] = symbol;
+            case 7 -> gameBoard[4][0] = symbol;
+            case 8 -> gameBoard[4][2] = symbol;
+            case 9 -> gameBoard[4][4] = symbol;
+        }
+    }
+
+    public static String CheckWinner() {
+        List<Integer> topRow = Arrays.asList(1, 2, 3);
+        List<Integer> midRow = Arrays.asList(4, 5, 6);
+        List<Integer> botRow = Arrays.asList(7, 8, 9);
+        List<Integer> leftCol = Arrays.asList(1, 4, 7);
+        List<Integer> midCol = Arrays.asList(2, 5, 8);
+        List<Integer> rightCol = Arrays.asList(3, 6, 9);
+        List<Integer> cross1 = Arrays.asList(3, 5, 7);
+        List<Integer> cross2 = Arrays.asList(1, 5, 9);
+
+        List<List> winning = new ArrayList<>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for (List list : winning) {
+            if (playerPositions.containsAll(list)) {
+                return "Congratulations, you are winner!";
+            } else if (cpuPositions.containsAll(list)) {
+                return "CPU wins! Sorry :(";
+            } else if (playerPositions.size() + cpuPositions.size() == 9) {
+                return "CAT";
+            }
+        }
+        return "";
+    }
 }
--- src/main/java/pl/oleksii/Main.java
+++ src/main/java/pl/oleksii/Main.java
@@ -1,5 +1,43 @@
-package pl.oleksii;public class Main {
+package pl.oleksii;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import static pl.oleksii.GameBoard.*;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        printGameBoard(gameBoard);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your placement: (1-9): ");
+            int playerPos = scanner.nextInt();
+            while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
+                System.out.println("Position taken! Enter a correct Position");
+                playerPos = scanner.nextInt();
+            }
+            PlacePiece(gameBoard, playerPos, "player");
+            String result = CheckWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+
+            Random random = new Random();
+            int cpuPos = random.nextInt(9) + 1;
+            while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
+                cpuPos = random.nextInt(9) + 1;
+            }
+            PlacePiece(gameBoard, cpuPos, "cpu");
+
+            printGameBoard(gameBoard);
+            result = CheckWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+            System.out.println(result);
+        }
     }
 }
